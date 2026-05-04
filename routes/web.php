2f9b2db2e52f60174826605c9cf8860c13fcd1b8
sssxzrsx <izpodvalavlog@gmail.com>
2026-05-04 09:51:02 +0500

    абрикосы ананасы а вы все пид***сы(5работа на месте)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\MainController;
+use App\Http\Controllers\Admin\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,3 +22,10 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin', 'namespace' => 'admin'], function() {
     Route::get('/', [MainController::class, 'index']) ->name('admin.index');
 });
+
+Route::group(['prefix'=> 'admin'], function() {
+Route::resource('categories', CategoryController::class);
+
+});
+
+
