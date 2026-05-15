baf229e13f6e8a28e69efc105b8c8b99c06ca8ea
sssxzrsx <izpodvalavlog@gmail.com>
2026-05-15 14:41:23 +0500

    Восьмая работа

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,15 +5,19 @@ namespace App\Models;
 use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class Post extends Model
 {
     use Sluggable;
     use HasFactory;
 
+    protected $fillable = ['title', 'description', 'content', 'category_id', 'thumbnail'];
+
     public function tags()
     {
-        return $this->belongsToMany(Tag::class);
+        return $this->belongsToMany(Tag::class)->withTimestamps();
     }
 
     public function category()
@@ -21,13 +25,32 @@ class Post extends Model
         return $this->belongsTo(Category::class);
     }
 
-
     public function sluggable(): array
     {
         return [
-            'slug'=> [
-                'source'=> 'title'
-                ]
-            ];
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+
+    public static function uploadImage(Request $request, $image = null)
+    {
+        if ($request->hasFile('thumbnail')) {
+            if ($image) {
+                Storage::disk('public')->delete($image);
+            }
+            $folder = date('Y-m-d');
+            return $request->file('thumbnail')->store("images/{$folder}", 'public');
+        }
+        return null;
+    }
+
+    public function getImage()
+    {
+        if (!$this->thumbnail) {
+            return asset('no-image.png');
+        }
+        return asset("uploads/{$this->thumbnail}");
     }
 }
